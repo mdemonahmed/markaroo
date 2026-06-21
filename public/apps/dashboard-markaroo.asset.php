5cb3a6bee32b97f43dea197a8cd1e6ade4caf997
Emon Ahmed <emonahmed2255@gmail.com>
2026-06-21 15:47:04 +0600

    frontend

--- public/apps/dashboard-markaroo.asset.php
+++ public/apps/dashboard-markaroo.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '59b4384875f5908dff44');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'f16f29f1975cfb8ea4d2');
