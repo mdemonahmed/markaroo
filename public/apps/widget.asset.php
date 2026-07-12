dd5a198e967310a94590ba99a1cb1d0b840c8280
Emon Ahmed <emonahmed2255@gmail.com>
2026-07-13 03:54:46 +0600

    LOGO CHANGED

--- public/apps/widget.asset.php
+++ public/apps/widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'ecc6f95c023bc469556c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'e20859533d3b067e05f2');
