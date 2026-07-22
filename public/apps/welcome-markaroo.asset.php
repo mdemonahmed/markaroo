5ae3f72d9d55b68565237ca01f013d0662f22597
Emon Ahmed <emonahmed2255@gmail.com>
2026-07-22 20:16:35 +0600

    Update asset versions and enhance deactivation CSS layout
    - Updated version numbers for deactivation, greet, and prism asset files to reflect recent changes.
    - Added CSS styles to improve the layout of the deactivation confirmation section, ensuring better alignment and presentation of elements.

--- public/apps/welcome-markaroo.asset.php
+++ public/apps/welcome-markaroo.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '6e2c27660febb8a1c028');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '647a5803fee203685a51');
--- public/apps/widget.asset.php
+++ public/apps/widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '31abe91f8e67e9ca60d6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'f4760970216ffaf5016c');
